d3c0feebfc4475442031d013158490fdc6eb2679
duneSpice97 <89211252+duneSpice97@users.noreply.github.com>
2021-11-06 15:59:08 -0600

    commit tree fa2d4ede405e8baab31af1256eb6a32a9f85f54e

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
+const record = [
+    {year: "2018", result: "L"},
+    {year: "2017", result: "W"},
+    {year: "2016", result: "N/A"}
+    //...
+  ]
 
+
+
+// superbowlWin(record);
+
+function superbowlWin(array) {
+    const wins = array.find(array => array.result === 'W');
+
+    if (wins) {
+        return wins.year;
+    }
+}
+
+superbowlWin(record);
